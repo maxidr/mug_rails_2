0dcfa98e7e5f6a45ffc5642b5009fba05272d95f
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-06-11 13:38:40 -0300

    Se genera el modelo "Item"

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+# fields: quantity
+class Item < ActiveRecord::Base
+  belongs_to :product
+  validates_presence_of :quantity, :product
+  validates_numericality_of :quantity, :only_integer => true, :greater_than => 0
+
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110611163048) do
+ActiveRecord::Schema.define(:version => 20110611163549) do
+
+  create_table "items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "name"
