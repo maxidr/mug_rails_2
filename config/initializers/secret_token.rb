b44e776b1ab5129892e595da1ba7f1c38f54002b
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-06-13 15:34:54 -0300

    Se eliminan directorios que pertenecian a otra rama

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Compras::Application.config.secret_token = '51b8fd0b34631ebc2887f90885cfc538ab69d11594da710058b64567b452df1b1513e60278ff550e1e4f7b86475d8bdba8564462b285e77db0b6fdf04db42280'
