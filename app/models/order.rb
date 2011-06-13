bb1fd5ea8b5d582decc96135abc5e8e39d6ec726
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2011-06-13 12:49:17 -0300

    Se agrega el monto total a order Se modifica el show de orden

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,9 @@
 class Order < ActiveRecord::Base
   has_many :items
   accepts_nested_attributes_for :items, :reject_if => proc { |attr| attr[:quantity].blank? }
+  
+  def total_amount
+    items.to_a.sum { |i| i.product.price * i.quantity }
+  end
 end
 
